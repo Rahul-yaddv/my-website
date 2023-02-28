a4609bf0ad06d5f835d0e6fc55bb9fb9bb54bae9
rahul-yaddv <rahul08yaduvanshi@gmail.com>
2023-02-28 06:10:29 +0530

    added 2 dep react-scrolls and git-icons and also some skelton code

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,19 @@
-import React from 'react';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from 'react-router-dom';
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: (
-      <div>
-        <h1 className="text-2xl">Hello World</h1>
-        <Link to="about">About Us</Link>
-      </div>
-    ),
-  },
-  {
-    path: 'about',
-    element: <div>About</div>,
-  },
-]);
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import Skills from './components/Skills';
+import Work from './components/Work';
+
 function App() {
   return (
     <div className="App">
-      <RouterProvider router={router}></RouterProvider>
+      <About />
+      <Contact />
+      <Home />
+      <NavBar />
+      <Skills />
+      <Work />
     </div>
   );
 }
---
+++ src/components/Skills.tsx
@@ -0,0 +1,5 @@
+function Skills() {
+  return <div>Skill</div>;
+}
+
+export default Skills;
